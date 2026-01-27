0b8ba764d73bbe1e4d46856fcc09b8de58cb2f97
SevaKarlov2005 <sevaneva05@gmail.com>
2026-01-27 17:53:52 +0300

    Assigning validators and dates to searchwindow.ui

--- searchwindow.cpp
+++ searchwindow.cpp
@@ -1,6 +1,8 @@
 #include "searchwindow.h"
 #include "ui_searchwindow.h"
 
+#include <QRegularExpressionValidator>
+
 SearchWindow::SearchWindow(QWidget *parent) : QMainWindow(parent)
 {
     // Установка интерфейса окна
@@ -9,6 +11,21 @@ SearchWindow::SearchWindow(QWidget *parent) : QMainWindow(parent)
 
     // Установка фиксированного размера окна
     setFixedSize(1045, 855);
+
+    // Регулярные выражения для валидаторов
+    QRegularExpression bid_id_pattern(R"([0-9]{5}-[0-9]{2}/ОМиДР)");
+    QRegularExpression date_pattern(R"((0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.[0-9]{2})");
+
+    // Установка валидатора для работы с заявкой
+    this->ui->bid_id_work_edit->setValidator(new QRegularExpressionValidator(bid_id_pattern));
+
+    // Установка валидаторов для фильтра заявок
+    this->ui->bid_id_filter_edit->setValidator(new QRegularExpressionValidator(bid_id_pattern));
+    this->ui->bid_date_filter_edit->setValidator(new QRegularExpressionValidator(date_pattern));
+    this->ui->bid_change_filter_edit->setValidator(new QRegularExpressionValidator(date_pattern));
+
+    // Установка валидатора для просмотра заявки
+    this->ui->bid_id_read_edit->setValidator(new QRegularExpressionValidator(bid_id_pattern));
 }
 
 SearchWindow::~SearchWindow()
