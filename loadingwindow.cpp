e9df27dd3576f4ff6494af137b25b0407c0892d9
SevaKarlov2005 <sevaneva05@gmail.com>
2026-01-27 21:13:16 +0300

    Assigning validators and dates to loadingwindow.ui

--- loadingwindow.cpp
+++ loadingwindow.cpp
@@ -1,6 +1,8 @@
 #include "loadingwindow.h"
 #include "ui_loadingwindow.h"
 
+#include <QRegularExpressionValidator>
+
 LoadingWindow::LoadingWindow(QWidget *parent) : QMainWindow(parent)
 {
     // Установка интерфейса окна
@@ -9,6 +11,30 @@ LoadingWindow::LoadingWindow(QWidget *parent) : QMainWindow(parent)
 
     // Установка фиксированного размера окна
     setFixedSize(1045, 855);
+
+    // Регулярные выражения для валидаторов
+    QRegularExpression code_pattern(R"([A-Z]{3})");
+    QRegularExpression item_pattern(R"([0-9]{6})");
+    QRegularExpression number_pattern(R"([0-9])");
+    QRegularExpression bid_id_pattern(R"([0-9]{5}-[0-9]{2}/ОМиДР)");
+
+    // Установка валидаторов для работы с контейнером
+    this->ui->code_work_edit->setValidator(new QRegularExpressionValidator(code_pattern));
+    this->ui->item_work_edit->setValidator(new QRegularExpressionValidator(item_pattern));
+    this->ui->number_work_edit->setValidator(new QRegularExpressionValidator(number_pattern));
+
+    // Установка валидаторов для фильтра контейнеров
+    this->ui->code_filter_edit->setValidator(new QRegularExpressionValidator(code_pattern));
+    this->ui->item_filter_edit->setValidator(new QRegularExpressionValidator(item_pattern));
+    this->ui->number_filter_edit->setValidator(new QRegularExpressionValidator(number_pattern));
+
+    // Установка валидаторов для просмотра контейнера
+    this->ui->code_read_edit->setValidator(new QRegularExpressionValidator(code_pattern));
+    this->ui->item_read_edit->setValidator(new QRegularExpressionValidator(item_pattern));
+    this->ui->number_read_edit->setValidator(new QRegularExpressionValidator(number_pattern));
+
+    // Установка валидатора для просмотра заявки
+    this->ui->bid_id_read_edit->setValidator(new QRegularExpressionValidator(bid_id_pattern));
 }
 
 LoadingWindow::~LoadingWindow()
